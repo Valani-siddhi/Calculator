58608c36461694d492797cbe3297b6effff4b6ff
Valani-siddhi <Siddhivalani1@gmail.com>
2025-07-24 10:29:41 +0530

    Delete project1 directory

--- project1/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS personality_test;
-USE personality_test;
-
-CREATE TABLE results (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    personality VARCHAR(4),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
